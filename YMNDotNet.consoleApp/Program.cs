bd8b598759d9873c11099a6797734fb83890a7c5
Yoon <yoonmyatnoe.ucs@gmail.com>
2026-07-08 23:12:31 +0630

    feat:SQlDataRead

--- YMNDotNet.consoleApp/Program.cs
+++ YMNDotNet.consoleApp/Program.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 Console.WriteLine("Hello, World!");
 
 Console.ReadKey();
-string connectionStr = "Source=.;Initial Catalog=YMNDotnet;User ID=sa;Password=ymn@123;";
+string connectionStr = "Data Source=.;Initial Catalog=YMNDotnet;User ID=sa;Password=ymn@123;";
 Console.WriteLine(value:"Connection String ="+ connectionStr);
 SqlConnection connection = new SqlConnection(connectionStr);
 
@@ -17,10 +17,29 @@ string query = @"SELECT [Blogid]
       ,[BlogConent]
   FROM [dbo].[Tbl_blog]";
 SqlCommand cmd = new SqlCommand(cmdText: query,connection);
-SqlDataAdapter adapter = new SqlDataAdapter(selectCommand: cmd);
+/*SqlDataAdapter adapter = new SqlDataAdapter(selectCommand: cmd);
 DataTable dt = new DataTable();
 adapter.Fill(dataTable: dt);
 connection.Close();
+
+foreach (DataRow row in dt.Rows)
+{
+    Console.WriteLine(value: row["Blogid"] + " " + row["BlogTitle"] + " " + row["BlogAuthor"] + " " + row["BlogConent"]);
+} 
+
+For Retrive the whole dataset and looping output each row
+*/
+
+SqlDataReader reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+       Console.WriteLine(value: reader["Blogid"] + " " + reader["BlogTitle"] + " " + reader["BlogAuthor"] + " " + reader["BlogConent"]);
+}connection.Close();
+
+
+
+
+
 
 
 
