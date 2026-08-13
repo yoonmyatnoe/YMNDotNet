d178f10daff4087073b9f2c9200d6218bb924216
User <User@DESKTOP-68SS8I4>
2026-08-13 23:17:19 +0630

    EFCore.Cs

---
+++ YMNDotNet.consoleApp/EFCoreExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMNDotNet.consoleApp
+{
+    public class EFCoreExample
+    {
+        public void Read()
+        {
+            AppDbContext db = new AppDbContext();
+            var blogs = db.Blogs.Where(x => x.DeletedFlag == false).ToList();
+            foreach (var item in blogs)
+            {
+                Console.WriteLine(item.BlogID);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogConent);
+                Console.WriteLine("-----------------");
+            }
+
+        }
+    }
+}
