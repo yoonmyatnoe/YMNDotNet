9f398f4c2e4f3f075c5b780ac579f6adc224fa14
User <User@DESKTOP-68SS8I4>
2026-08-13 23:16:39 +0630

    EFCore Configure

---
+++ YMNDotNet.consoleApp/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMNDotNet.consoleApp.Models;
+
+namespace YMNDotNet.consoleApp
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Data Source=.;Initial Catalog=YMNDotnet;User ID=sa;Password=9@1@84;TrustServerCertificate=True;";
+                optionsBuilder.UseSqlServer(connectionString);
+
+            }
+
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+
+    }
+}
