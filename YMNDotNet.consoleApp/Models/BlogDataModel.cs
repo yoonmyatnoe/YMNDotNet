3897d5a1af866b491f51477c4fdb028ac32e3f8c
User <User@DESKTOP-68SS8I4>
2026-08-13 23:16:14 +0630

    DataModel:Dapper,EFcore

---
+++ YMNDotNet.consoleApp/Models/BlogDataModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMNDotNet.consoleApp.Models
+{
+    public class BlogDapperDataModel
+    {
+        public int BlogID { get; set; }
+
+        public string BlogTitle { get; set; }
+
+        public string BlogAuthor { get; set; }
+
+        public string BlogConent { get; set; }
+    }
+
+    [Table("Tbl_blog")]
+    public class BlogDataModel
+    {
+        [Key]
+        [Column("Blogid")]
+        public int BlogID { get; set; }
+
+        [Column("BlogTitle")]
+        public string BlogTitle { get; set; }
+
+        [Column("BlogAuthor")]
+        public string BlogAuthor { get; set; }
+
+        [Column("BlogConent")]
+        public string BlogConent { get; set; }
+
+        [Column("DeletedFlag")]
+        public long DeletedFlag { get; set; }
+    }
+}
